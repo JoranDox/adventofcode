4d527fc9d3189e7106f782f1cdac6646adbf45d4
Joran Dox <joran.dox@gmail.com>
2021-12-06 08:38:02 +0100

    added naive way for the lulz

--- 2021/day06.py
+++ 2021/day06.py
@@ -12,8 +12,9 @@ print(c[3])
 nbabies = c[0]
 cnew = c
 
+# for day in range(81):
 for day in range(257):
-    
+
     cnew.update({
         6: nbabies,
         8: nbabies
@@ -26,8 +27,22 @@ for day in range(257):
             i-1: k for i,k in c.items() if i and k
         }
     )
-    # print(cnew)
 
-    # new day here
+print(c.total())
 
-    print(c.total())
+# naive way, works for 80 but takes insanely long for 256:
+
+data2 = [d for d in data]
+for day in range(80):
+# import tqdm
+# for day in tqdm.tqdm(range(256)): # this takes too long
+# 65%|██████▌   | 167/256 [13:44<1:26:16, 58.17s/it]
+    datanew = []
+    for d in data2:
+        if d == 0:
+            datanew.append(6)
+            datanew.append(8)
+        else:
+            datanew.append(d-1)
+    data2 = datanew
+    print(len(data2))
