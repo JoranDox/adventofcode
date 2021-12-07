19359bde3ec48756504f139fec81587ce78d5421
Joran Dox <joran.dox@gmail.com>
2021-12-07 16:11:57 +0100

    some meddling with medians & averages for day7

--- 2021/day07.py
+++ 2021/day07.py
@@ -3,18 +3,39 @@ with open("day07input.txt") as f:
     data = list(map(int,f.read().split(",")))
 
 # part 1
+besti = -1
 m = 10000000000000000
 for i in range(max(data)+1):
     s = sum([(abs(i-k)) for k in data])
-    m = min(m,s)
-print(i, s, m)
+    if s < m:
+        m = s
+        besti = i
+print(besti, m)
 
 # part 2
+besti = -1
+
 m = 10000000000000000
 for i in range(max(data)+1):
     # s = sum([sum(range(abs(i-k)+1)) for k in data])
     s = sum([((abs(i-k) * (abs(i-k)+1)//2)) for k in data])
-    m = min(m,s)
-print(i, s, m)
+    if s < m:
+        m = s
+        besti = i
+print(besti, m)
 
 # print(data)
+mediani = sorted(data)[len(data)//2]
+medians = sum([(abs(mediani-k)) for k in data])
+
+print(mediani, medians)
+import numpy as np
+avgi = np.round(sum(data) / len(data))
+avgs = list(map(
+    lambda i: sum([((abs(i-k) * (abs(i-k)+1)//2)) for k in data]),
+    (avgi-1, avgi, avgi+1, avgi+2)
+))
+
+avgi = int(np.round(sum(data) / len(data)))
+avgs = sum([((abs(i-k) * (abs(i-k)+1)//2)) for k in data])
+print(avgi, avgs)
