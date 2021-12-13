972f95d41fc17190392c8c3260985b99cc315280
Joran Dox <joran.dox@gmail.com>
2021-12-13 06:16:40 +0100

    day 13

---
+++ 2021/day13.py
@@ -0,0 +1,45 @@
+
+# with open("day13inputtest.txt") as f:
+with open("day13input.txt") as f:
+    data = f.read().strip()
+
+
+dots, fold = data.split("\n\n")
+
+dots = {
+    tuple([int(t) for t in d.split(",")]) for d in dots.splitlines()
+}
+# print(dots)
+# print(fold)
+
+def hmirror(x,y, xmirror):
+    return ((2*xmirror)-x,y) if (x > xmirror) else (x,y)
+def vmirror(x,y, ymirror):
+    return (x, (2*ymirror)-y) if (y > ymirror) else (x,y)
+
+firstpart = True
+for line in fold.splitlines():
+    if not line:
+        break
+    d,n = line.split("fold along ")[1].split("=")
+    if d == "y":
+        dots = {vmirror(*dot,int(n)) for dot in dots}
+    if d == "x":
+        dots = {hmirror(*dot,int(n)) for dot in dots}
+    if firstpart:
+        print("part 1:", len(dots))
+        firstpart = False
+# print(sorted(dots))
+# print(len(dots))
+
+printdata = [
+    [
+        ' ' for _ in range(max([i for i,_ in dots])+1) # max x
+    ] for _ in range(max([i for _,i in dots])+1) # max y
+]
+
+for i,j in dots:
+    printdata[j][i] = '#'
+
+print('part 2:')
+print("\n".join([''.join(p) for p in printdata]))
