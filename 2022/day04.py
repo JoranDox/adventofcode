2d973be7cbeef8c95c426d3befd99bc0334b36c2
joran.dox <joran.dox@gmail.com>
2022-12-04 06:26:44 +0100

    day4

---
+++ 2022/day04.py
@@ -0,0 +1,25 @@
+
+import pathlib
+parent_directory = pathlib.Path(__file__).resolve().absolute().parent
+# with open(parent_directory.joinpath("day04inputtest.txt")) as f:
+with open(parent_directory.joinpath("day04input.txt")) as f:
+    data = f.read().strip()
+
+counter = 0
+counter2 = 0
+for line in data.splitlines():
+#for line in data.split("\n\n"):
+    a,b = line.split(",")
+    a_start, a_stop = [int(k) for k in a.split("-")]
+    b_start, b_stop = [int(k) for k in b.split("-")]
+    aset = set(range(a_start, a_stop + 1))
+    bset = set(range(b_start, b_stop + 1))
+    # print(aset, bset)
+    if aset >= bset or bset >= aset:
+        counter += 1
+
+    if aset & bset:
+        counter2 += 1
+print(counter)
+print(counter2)
+
