2f0732c3c6d49ef06683930429ee1b5af5d1927c
Joran Dox <joran.dox@gmail.com>
2023-12-09 06:55:59 +0100

    cleaner day 9

--- 2023/day09.py
+++ 2023/day09.py
@@ -10,24 +10,27 @@ def extrapolate(xs):
     diffs = [x2 - x1 for x1, x2 in zip(xs[:-1], xs[1:])]
     # print("diffs", diffs)
     if any(diffs):
-        ext = extrapolate(diffs) + diffs[-1]
-        print("diffs", diffs, ext)
+        ext = extrapolate(diffs) + xs[-1]
+        print("diffs", xs, ext)
         return ext
     else:
+        # ext = extrapolate(diffs) + diffs[-1]
+        print("diffs", xs, xs[-1])
         print("diffs", diffs, 0)
-        return 0
+        return xs[-1]
 
 
 def extrapolatebackwards(xs):
     diffs = [x2 - x1 for x1, x2 in zip(xs[:-1], xs[1:])]
-    print(diffs)
+    # print(diffs)
     if any(diffs):
-        ext = diffs[0]- extrapolatebackwards(diffs)
-        print("diffs", ext, diffs)
+        ext = xs[0] - extrapolatebackwards(diffs)
+        print("diffs", ext, xs)
         return ext
     else:
+        print("diffs", xs[0], xs)
         print("diffs", 0, diffs)
-        return 0
+        return xs[0]
 
 
 accum = 0
@@ -37,10 +40,10 @@ for line in data.splitlines():
     line_int = [int(x) for x in line.split()]
     print(line_int)
     print("forwards")
-    p = extrapolate(line_int) + line_int[-1]
+    p = extrapolate(line_int)
     print("diffs", line_int, p)
     print("back")
-    p2 = line_int[0] - extrapolatebackwards(line_int) 
+    p2 = extrapolatebackwards(line_int)
     print("diffs", p2, line_int)
     accum += p
     accum2 += p2
