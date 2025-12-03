e976171a80550b7dff3e2f43e583265e449c6203
Joran Dox <joran.dox@mediahuis.be>
2025-12-03 08:30:59 +0100

    2025 day 3

--- 2025/day02.py
+++ 2025/day02.py
@@ -2,26 +2,26 @@ import re
 
 import pathlib
 aoc_dir = pathlib.Path(__file__).resolve().absolute().parent.parent
-# with open(aoc_dir.joinpath("input/2025/day02inputtest.txt")) as f:
-with open(aoc_dir.joinpath("input/2025/day02input.txt")) as f:
+with open(aoc_dir.joinpath("input/2025/day02inputtest.txt")) as f:
+# with open(aoc_dir.joinpath("input/2025/day02input.txt")) as f:
     data = f.read().strip()
 
 counter = 0
 summer = 0
 part2summer = 0
 # for line in data.splitlines():
-for line in data.split(","):
-    start, end = line.strip().split("-", 2)
+# for line in data.split(","):
+#     start, end = line.strip().split("-", 2)
 
-    start = int(start)
-    end = int(end)
-    for n in range(start, end + 1):
-        s = str(n)
-        l = len(s)
-        if s[:l//2] == s[l//2:]:
-            counter += 1
-            summer += n
-            print(n, "is doubled", f"count={counter}", f"sum={summer}")
+#     start = int(start)
+#     end = int(end)
+#     for n in range(start, end + 1):
+#         s = str(n)
+#         l = len(s)
+#         if s[:l//2] == s[l//2:]:
+#             counter += 1
+#             summer += n
+#             print(n, "is doubled", f"count={counter}", f"sum={summer}")
 
 print("part2")
 
